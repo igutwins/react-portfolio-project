e61a4b46bb443d8b1777b9d64b3c03c5c1fcc5c6
Ian Gutwinski <iangutwinski@gmail.com>
2021-06-20 15:26:00 -0400

    initial commit

---
+++ backend/db/migrate/20210620191812_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.string :assignee
+      t.string :reviewer
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
